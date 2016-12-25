53c5eec9bc0dc1a187089543d8bf7723da443a0e
Jason Data <jason.data@roadbytes.me>
2016-12-25 21:23:20 +1000

    User Show displays suggested Baby Names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    id    = params[:id].to_i
+    @user = User.find(id)
+  end
+
   def create
     @user = User.new(user_params)
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # This models users/people of the app
 class User < ActiveRecord::Base
   validates :email, presence: true
+  has_many  :baby_names
 
   has_secure_password
 
