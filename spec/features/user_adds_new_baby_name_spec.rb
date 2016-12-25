5b544ea94bdcdeae3b3130a0e53de20f60255f03
Jason Data <jason.data@roadbytes.me>
2016-12-25 11:42:09 +1000

    Add Nav and logged_in? method

--- spec/features/user_adds_new_baby_name_spec.rb
+++ spec/features/user_adds_new_baby_name_spec.rb
@@ -4,19 +4,19 @@ feature 'User adds new BabyName', type: 'feature' do
   scenario 'with valid data' do
     navigate_to_new_baby_name_form
 
-    fill_in_new_baby_name_form_incorrectly
-
-    verify_database_empty
-    verify_redirected_page
-  end
-
-  scenario 'with valid data' do
-    navigate_to_new_baby_name_form
-
     fill_in_new_baby_name_form
 
     verify_database
     verify_index_page
+  end
+
+  scenario 'with invalid data' do
+    navigate_to_new_baby_name_form
+
+    fill_in_new_baby_name_form_incorrectly
+
+    verify_database_empty
+    verify_redirected_page
   end
 
   def navigate_to_new_baby_name_form
