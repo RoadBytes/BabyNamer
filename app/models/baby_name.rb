410150c2b5bcd4a8a0e8d9daa29316a4cc638b37
Jason Data <jason.data@roadbytes.me>
2016-12-26 10:11:34 +1000

    style fix

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -2,7 +2,7 @@
 class BabyName < ActiveRecord::Base
   include VoteGemJay
 
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   belongs_to :user
 
   def user_handle
